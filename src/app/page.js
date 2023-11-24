c299783860ea57abecaa829c459f6fda2de7e9ae
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-11-24 17:43:34 +0100

    Just added in the final features

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
+
 'use client'
 
-import {useState} from 'react'; 
+import {useState, useEffect, useCallback, useRef} from 'react'
 import axios from 'axios'; 
+import DropDown from './Components/Dropdown'; 
+
+
 
 export default function Home() {
 
 const [input, setInput] = useState(''); 
 const [data, setData] = useState(); 
+const [coindeskData, setCoindeskData] = useState(null);
 
 var dataObj = {
   name: "Alice",
@@ -31,7 +36,7 @@ function sendData() {
 
   const textInput = 'input'; 
   dataObj[textInput] = input;
-  
+
   
   const data = dataObj;
   console.log(data); 
@@ -44,13 +49,49 @@ function sendData() {
       alert(JSON.stringify(response.data));
       console.log('data sent'); 
       
-    })
-    .catch((error) => {
+      
+    }).catch((error) => {
       alert(error.message);
       alert("this DID NOT WORK")
       console.error("this did not work")
     });
 }
+
+
+
+function getData() {
+  // Define the URL to make the GET request
+  const url = 'http://localhost:5000/server/coindesk';
+
+  // Use axios to make a GET request to the server
+  axios.get(url)
+    .then((response) => {
+      // Get the data from the response
+      console.log('trying to get data');
+      
+      const data = response.data;
+
+      // Set the coindesk data state variable to the data
+      setCoindeskData(data);
+    }).catch((error) => {
+      // Handle the error
+      // For example, alert the error message
+      console.log('this did not work');
+      console.error("this did not work")
+      alert(error.message);
+    });
+}
+
+
+function handleSelect(value) {
+  // Update the input state variable with the selected value
+  setInput(value);
+
+  // Do anything else you want with the selected value
+  // For example, send it to the server or display it on the web page
+}
+
+
 
 
 
@@ -63,8 +104,23 @@ function sendData() {
      <h1>
       SEND INPUT 
       
+      
      </h1>
      </button>
+     
+     <button onClick={getData} >
+      <h1>GET DATA</h1>
+     </button>
+     <div 
+     style={{color: 'black', background: 'white'}} 
+     >
+     <DropDown  
+     onSelect={handleSelect}
+     style={{color: 'black', background: 'white'}} 
+     
+     /> 
+     </div>
+     
      
     </main>
   )
